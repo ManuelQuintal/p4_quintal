8641f907c821522fc1cda69432f7e35b1b65ec88
ManuelQuintal <manuel199823@hotmail.com>
2021-11-17 15:39:49 -0500

    p4d1

---
+++ p4d1.py
@@ -0,0 +1,9 @@
+
+#%%
+import sys
+!{sys.executable} -m pip install scikit-learn
+# %%
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.naive_bayes import GaussianNB
+from sklearn import metrics
